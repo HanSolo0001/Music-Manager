c1580b98177a60bc05b9d4095b14e80917fc5451
HanSolo0001 <pillarite@gmail.com>
2017-01-26 19:40:28 -0500

    changed default information on pages to match up with music manager project

--- Music Manager/Controllers/HomeController.cs
+++ Music Manager/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Music_Manager.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Music Manager is a web app that gives users access to a dynamic music library that lets you add, delete, or modify current artists and song selections in a given playlist. You can also create, delete, or edit a current playlist. In order to use the Music Manager app, please login.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Use this page to contact the developer of this app with any questions or feedback.";
 
             return View();
         }
